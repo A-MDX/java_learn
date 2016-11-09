d70bb175656c8587ef8a78faa94e85dbf9db2702
a-mdx <634187415@qq.com>
2016-11-09 17:19:37 +0800

    ...代码太多，头疼

--- design_model/src/templateMethod/TemplateTest.java
+++ design_model/src/templateMethod/TemplateTest.java
@@ -13,6 +13,24 @@ public class TemplateTest {
         AbstractCal cal = new Plus();
         int result = cal.calculate(exp,"\\+");
         System.out.println(exp + " --> "+result);
+        
+        String sql = "SELECT \n" +
+                "  g.`group_code`,\n" +
+                "  g.`creationtime`,\n" +
+                "  g.`enablestate`,\n" +
+                "  g.`post_url`,\n" +
+                "  g.`post_key`,\n" +
+                "  g.`name`,\n" +
+                "  fc.`code_name` enablestate_str \n" +
+                "FROM\n" +
+                "  org_group g \n" +
+                "  LEFT JOIN tm_fix_code fc \n" +
+                "    ON fc.`code_type` = 1012 \n" +
+                "    AND fc.`code` = g.`enablestate` \n" +
+                "    WHERE 1 = 1\n" +
+                "    AND g.`name` LIKE '%广%'\n" +
+                "    AND g.`group_code` LIKE 'T'";
+        
     }
     
 }
