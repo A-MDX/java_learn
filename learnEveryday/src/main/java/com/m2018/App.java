4e77dcee7d304bf34b344544ef268e5d7c52c595
A-MDX <634187415@qq.com>
2018-01-04 00:37:13 +0800

    想做一个微信跳一跳辅助工具，好困啊，只能明天做了

---
+++ learnEveryday/src/main/java/com/m2018/App.java
@@ -0,0 +1,7 @@
+package com.m2018;
+
+/**
+ * Create by A-mdx at 2018/1/3 22:08
+ */
+public class App {
+}
---
+++ learnEveryday/src/main/java/com/m2018/january/January03/Tiaoyitiao.java
@@ -0,0 +1,15 @@
+package com.m2018.january.January03;
+
+/**
+ * Create by A-mdx at 2018/1/3 22:14
+ */
+public class Tiaoyitiao {
+    // 安卓命令，让其截图
+    // adb shell screencap /sdcard/mi/m.png
+    /*
+    // 拉出来
+    adb pull /sdcard/mi/m.png C:\Users\63418\Desktop\hehe
+     */
+    
+    
+}
