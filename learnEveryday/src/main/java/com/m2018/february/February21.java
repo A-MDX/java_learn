332a236458cfe495323affb5a259010a88b361b2
A-MDX <634187415@qq.com>
2018-02-27 23:42:40 +0800

    这是陪女友在医院写的，一点点，很有趣

---
+++ learnEveryday/src/main/java/com/m2018/february/February21.java
@@ -0,0 +1,16 @@
+package com.m2018.february;
+
+/**
+ * Created By a-mdx on 2018/2/21 下午2:32
+ */
+public class February21 {
+    public static void main(String... args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a|b:" + (a | b));
+        System.out.println("a&b:" + (a & b));
+        System.out.println("a^b:" + (a ^ b));
+
+
+    }
+}
